e6328ee6c9f64d5dcead1e36831fda8a09f6511c
talwarabhimanyu <talwarabhimanyu@users.noreply.github.com>
2021-05-02 19:42:36 +0000

    deploy: eacdf5a520f34c5788aaca4727c2bdc5a806a2e8

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/paper-notes-v1/assets/js/search-data.json', true);
+    request.open('GET', '/paper-notes/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
